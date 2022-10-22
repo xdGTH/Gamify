47018aa1a0a7f82df4c907051a93239bd4771a44
xdGTH <gotohell.heaven45@gmail.com>
2022-10-22 13:36:46 +0545

    initial commit

---
+++ android/app/src/main/kotlin/com/example/livepj/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.livepj
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/FixtureScreen.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:livepj/widgets/LaLiga.dart';
+import 'package:livepj/widgets/PremierLeague.dart';
+import 'package:livepj/widgets/UEFA.dart';
+import 'package:livepj/HomeScreen.dart';
+
+class FixtureScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 6,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: PreferredSize(
+            child: TabBar(
+              isScrollable: true,
+              unselectedLabelColor: Colors.white.withOpacity(0.3),
+              indicatorColor: Colors.white,
+              tabs: [
+                Tab(
+                  child: Text('17 May'),
+                ),
+                Tab(
+                  child: Text('18 May'),
+                ),
+                Tab(
+                  child: Text('19 May'),
+                ),
+                Tab(
+                  child: Text('Today'),
+                ),
+                Tab(
+                  child: Text('21 May'),
+                ),
+                Tab(
+                  child: Text('22 May'),
+                ),
+              ],
+            ),
+            preferredSize: Size.fromHeight(20.0),
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: SizedBox(
+            height: 2000,
+            child: TabBarView(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: 13.0, right: 13.0, bottom: 13.0, top: 25),
+                  child: Column(
+                    children: [
+                      PremierLeague(),
+                      LaLiga(),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: 13.0, right: 13.0, bottom: 13.0, top: 25),
+                  child: Column(
+                    children: [
+                      PremierLeague(),
+                      LaLiga(),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: 13.0, right: 13.0, bottom: 13.0, top: 25),
+                  child: Column(
+                    children: [
+                      PremierLeague(),
+                      LaLiga(),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: 13.0, right: 13.0, bottom: 13.0, top: 25),
+                  child: Column(
+                    children: [
+                      PremierLeague(),
+                      LaLiga(),
+                      UEFA(),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: 13.0, right: 13.0, bottom: 13.0, top: 25),
+                  child: Column(
+                    children: [
+                      PremierLeague(),
+                      LaLiga(),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: 13.0, right: 13.0, bottom: 13.0, top: 25),
+                  child: Column(
+                    children: [
+                      PremierLeague(),
+                      LaLiga(),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
